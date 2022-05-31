cde061c6ca243642e01a63d89408899bd2e77da5
Mahinayo <mahaletyohannesghgit commit -m 'readme'@gmail.com>
2022-05-31 02:17:54 -0700

    edges

---
+++ 0x00-python-hello_world/7-edges.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+word = "Holberton"
+word_first_3 = word[:3]
+work_last_2 = work[-2:]
+middle_word = work[1:-1]
+print(f"First 3 letters: {word_first_3}")
+print(f"Last 2 letters: {word_last_2}")
+print(f"Middle word: {middle_word}")
