a7c7d33e4c90a9a1e4718b5bbf6f146ab568342d
Mahinayo <99645691+Mahinayo@users.noreply.github.com>
2022-06-01 16:58:40 +0300

    Update 101-remove_char_at.py

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def remove_char_at(str, n):
     if n >= 0:
-        istr = str[:n] + str[n + 1:]
-        return (istr)
+        i_str = str[:n] + str[n + 1:]
+        return (i_str)
     else:
-        return (istr)
+        return (str)
