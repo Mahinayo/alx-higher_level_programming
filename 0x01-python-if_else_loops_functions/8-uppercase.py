43e2c2ffb23d33f5ad0b987fa580ae777d47760a
Mahinayo <99645691+Mahinayo@users.noreply.github.com>
2022-06-01 16:36:03 +0300

    Create 8-uppercase.py

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for i in str:
+        if ord('a') <= ord(c) <= ord('z'):
+            i = chr(ord(c) - (ord('a') - ord('A')))
+        print("{:s}".format(c), end='')
+    print("")
