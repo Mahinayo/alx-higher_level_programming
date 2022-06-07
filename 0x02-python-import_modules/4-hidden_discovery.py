0ff2cfd65ef0d609e0c9801d993f2057de0f4127
Mahinayo <99645691+Mahinayo@users.noreply.github.com>
2022-06-07 10:10:13 +0300

    Create 4-hidden_discovery.py

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import hidden_4
+
+
+def principal():
+    for x in dir(hidden_4):
+        if not (x[0] == '_' and x[1] == '_'):
+            print(x)
+
+
+if __name__ == "__main__":
+    principal()
