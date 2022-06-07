b50b2703fb223c329cffc0cdd4e621d1d595254c
Mahinayo <99645691+Mahinayo@users.noreply.github.com>
2022-06-07 10:00:48 +0300

    Update 3-infinite_add.py

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     from sys import argv
-    x = 0
-    for y in argv[1:]:
-        x += int(y)
-    print("{:d}".format(x))
+    add = 0
+    for s in argv[1:]:
+        add += int(s)
+    print("{:d}".format(add))
