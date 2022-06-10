cf54f1c8e4c1058b2ff68c7e55cc30c2c24681a7
Mahinayo <99645691+Mahinayo@users.noreply.github.com>
2022-06-10 10:02:03 +0300

    Create 12-roman_to_int.py

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    x = {'M': 1000, 'D': 500, 'C': 100, 'L': 50, 'X': 10, 'V': 5, 'I': 1}
+    y = 0
+    total = 0
+    if isinstance(roman_string, str):
+        for y in range(len(roman_string) - 1):
+            if x[roman_string[i]] >= x[roman_string[y + 1]]:
+                total += x[roman_string[y]]
+            else:
+                total -= x[roman_string[y]]
+            y += 1
+        total += x[roman_string[y]]
+        return total
+    else:
+        return 0
